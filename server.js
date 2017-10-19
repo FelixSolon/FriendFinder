69ec2439a5aeeb8fb3b67e850e44db0e23a08faf
Eric Stanulis <erstanulis@gmail.com>
2017-10-19 15:34:26 -0500

    Got submit button working

--- server.js
+++ server.js
@@ -1,5 +1,32 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-require('http');
 
+// Require/import the HTTP module
+var http = require("http");
+
+// Define a port to listen for incoming requests
+var PORT = 8080;
+
+var dataholder = "";
+
+// Create a generic function to handle requests and responses
+function handleRequest(request, response) {
+    request.on("data", function(data){
+        dataholder+=data;
+        console.log(dataholder);
+    })
+  // Send the below string to the client when the user visits the PORT URL
+  response.end("It Works!! Path Hit: " + request.url);
+}
+
+// Use the Node HTTP package to create our server.
+// Pass the handleRequest function to empower it with functionality.
+var server = http.createServer(handleRequest);
+
+// Start our server so that it can begin listening to client requests.
+server.listen(PORT, function() {
+
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
