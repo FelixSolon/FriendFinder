50f763763a1c47ef16b95f1a1f36f050f1a72639
Eric Stanulis <erstanulis@gmail.com>
2017-10-19 16:04:14 -0500

    Got basic server working

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
+//I feel like I'm doing cargo cult coding at the moment.
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+})
+
+app.get("/survey.html", function(req, res){
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+})
+
 // Require/import the HTTP module
 var http = require("http");
 
@@ -11,21 +24,13 @@ var PORT = 8080;
 var dataholder = "";
 
 // Create a generic function to handle requests and responses
-function handleRequest(request, response) {
-    request.on("data", function(data){
-        dataholder+=data;
-        console.log(dataholder);
-    })
-  // Send the below string to the client when the user visits the PORT URL
-  response.end("It Works!! Path Hit: " + request.url);
-}
+
 
 // Use the Node HTTP package to create our server.
 // Pass the handleRequest function to empower it with functionality.
-var server = http.createServer(handleRequest);
 
 // Start our server so that it can begin listening to client requests.
-server.listen(PORT, function() {
+app.listen(PORT, function() {
 
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
